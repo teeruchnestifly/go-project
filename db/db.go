e1e20309e755c4a7a5b1148491f36ada11cac372
Teeuch <teeruch@nestifly.com>
2023-03-19 15:17:51 +0700

    fixed bug cannot find applicion.yaml config file

--- db/db.go
+++ db/db.go
@@ -2,19 +2,53 @@ package db
 
 import (
 	"fmt"
+	"os"
 
+	"gopkg.in/yaml.v3"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Config struct {
+	Database struct {
+		Driver   string `yaml:"driver"`
+		Host     string `yaml:"host"`
+		Port     int    `yaml:"port"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		DBName   string `yaml:"dbname"`
+	} `yaml:"database"`
+}
+
 var DB *gorm.DB
 
 func Connect() {
-	// Set up database connection
-	dsn := "root:password@tcp(localhost:3306)/nestifly?charset=utf8mb4&parseTime=True&loc=Local"
-	var err error
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	configFile, err := os.Open("application.yaml")
 	if err != nil {
+		panic(err)
+	}
+	defer configFile.Close()
+
+	var config Config
+	err = yaml.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Connect to database using GORM
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.Database.User,
+		config.Database.Password,
+		config.Database.Host,
+		config.Database.Port,
+		config.Database.DBName,
+	)
+
+	// Set up database connection
+	// dsn := "root:password@tcp(localhost:3306)/nestifly?charset=utf8mb4&parseTime=True&loc=Local"
+	var err2 error
+	DB, err2 = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err2 != nil {
 		fmt.Println(err)
 		return
 	}
